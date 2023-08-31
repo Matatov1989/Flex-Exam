4b4ce6a75be3f054bdaa9fd258839fb71930d915
Yury <matatov1989@gmail.com>
2023-08-31 16:40:10 +0300

    finish task

--- app/src/main/java/com/example/flexexam/util/Constants.kt
+++ app/src/main/java/com/example/flexexam/util/Constants.kt
@@ -1,7 +1,7 @@
 package com.example.flexexam.util
 
 object Constants {
-    const val MOVIE_API_KEY = "2c46288716a18fb7aadcc2a801f3fc6b"
+    const val MOVIE_API_KEY = "d2bc56bb74d10fcca04542127ebda98c"
     const val BASE_MOVIE_URL = "https://api.themoviedb.org/3/"
     const val POSTER_IMG_URL = "https://image.tmdb.org/t/p/w500/"
     const val MOVIE_DB = "Movie_DB"
